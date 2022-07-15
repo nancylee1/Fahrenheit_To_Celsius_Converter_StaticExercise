1527898da5d890643280672b497063c31548de9d
Nancy Lee <107009879+nancylee1@users.noreply.github.com>
2022-07-15 14:51:10 -0400

    init commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using StaticExercise;
+
+var celsius = TempConverter.FahrenheitToCelsius(100);
+var fahrenheit = TempConverter.CelsiusToFahrenheit(0);
+
+Console.WriteLine("Temperature Conversion - Fahrenheit || Celsius");
+Console.WriteLine("-----------------------------------------------------\n");
+Console.WriteLine($"°F to °C = {celsius}");
+Console.WriteLine($"°C to °F = {fahrenheit}");
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("USER INPUT - Temperature Conversion - Fahrenheit || Celsius");
+Console.WriteLine("-----------------------------------------------------\n");
+Console.Write($"What temperature in Fahrenheit would you like to convert to Celsius? "); 
+double x = double.Parse(Console.ReadLine()); 
+Console.WriteLine($"The conversion of your Fahrenheit to Celsius of {x} degrees is {celsius} Celsius");
+Console.WriteLine();
+
+Console.Write($"What temperature in Celsius would you like to convert to Fahrenheit? "); 
+double y = double.Parse(Console.ReadLine());
+Console.WriteLine($"The conversion of your Celsius to Fahrenheit of {y} degrees is {fahrenheit} Fahrenheit");
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StaticExercise; 
+
+//Exercise 1:
+//Create a new console app called StaticExercise.
+
+//Now create a static class called TempConverter.The class should have at least 2 methods, one called FahrenheitToCelsius that will require a double as a parameter
+//and return a double, the other CelsiusToFahrenheit which will also require a double as a parameter and return a double.
+
+//Fill out these methods and call them in your Program.cs file. Make sure they’re accurate!
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius (double fahrenheit) 
+        {
+            return (fahrenheit - 32) / 1.8;
+        }
+
+        public static double CelsiusToFahrenheit (double celsius)
+        {
+            return (celsius * 9) / 5 + 32;
+        }
+    }
+}
